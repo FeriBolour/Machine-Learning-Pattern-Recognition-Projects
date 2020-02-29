9af90c0f718481975351aca269e45e4b07daa62d
FeriBolour <57817579+FeriBolour@users.noreply.github.com>
2020-02-28 19:06:03 -0600

    idk

--- Farshad_Bolouri_ML_Project3.m
+++ Farshad_Bolouri_ML_Project3.m
@@ -4,22 +4,20 @@ close all
 
 rng(41);
 
-D = GenerateDataset(25,100);
+[D ,X_test ,T_test] = GenerateDataset(25,100,1000);
 lambda = 0.08:4.415/35:4.9;
-phi = phiCal(D,100,25);
-[W_average, Y, W] = Predict(D,phi,lambda,25);
-%  for i=1:length(Y)
-%      pltLine(D,Y{1},Y_average(:,1));
-%  end
-% hold on
-% plot(0:1/24:1,Y_average(:,1),'Color','black','LineWidth',1.5);
+[phi, phi_test] = phiCal(D,100,25,X_test);
+[W_average, Y, W, Y_test] = Predict(D,phi,lambda,25,phi_test);
 
-[BiasSq Var BiasSq_Var] = BiasVarCal(W_average,W,D,25,phi,Y);
+Test_Error = CalError(Y_test,T_test,X_test);
 
-plt(BiasSq,Var,BiasSq_Var,lambda);
+[BiasSq, Var, BiasSq_Var] = BiasVarCal(W_average,W,D,25,phi,Y);
+
+%plt(BiasSq,Var,BiasSq_Var, Test_Error, lambda);
+plt(BiasSq,Var,BiasSq_Var, lambda);
 
 %% Generate Dataset
-function D = GenerateDataset(N,L)
+function [D ,X_test ,T_test] = GenerateDataset(N,L,testN)
     D = cell(1,L);
     
     for i =1:L
@@ -27,14 +25,18 @@ function D = GenerateDataset(N,L)
         D(i) = mat2cell([X_train,(sin(2*pi*X_train)...
             + mvnrnd(0,0.3,N))],N,2);
     end
+    X_test = rand(testN,1);
+    T_test = sin(2*pi*X_test) + mvnrnd(0,0.3,testN);
 end
 
 %% phiCal: This function calculates Design Matrix (phi)
-function phi = phiCal(D,L,N)
-phi_train = ones(length(cell2mat(D(1))),N);
+function [phi ,phi_test] = phiCal(D,L,N,X_test)
+
 phi= cell(1,L);
 
 Mu = 0:1/13:1;
+phi_train = ones(length(cell2mat(D(1))),length(Mu));
+phi_test = ones(length(X_test),length(Mu));
 
 for i =1:L
     X = cell2mat(D(i));
@@ -42,31 +44,38 @@ for i =1:L
     for j =2:14
         phi_train(:,j) = exp(-(X-Mu(j-1)).^2/(2*(0.1^2)));
     end
-    phi(i) = mat2cell(phi_train,N,N);
+    phi(i) = mat2cell(phi_train,N,length(Mu));
 end
+
+for j = 2:14
+    phi_test(:,j) = exp(-(X_test-Mu(j-1)).^2/(2*(0.1^2)));
+end
+
 end
 %% Predict function:
 %this function does the predictions for each lambda and then averages them
-function [W_average,Y, W] = Predict(D, phi,lambda,N)
-    W_average = zeros(N,length(lambda));
+function [W_average,Y, W, Y_test] = Predict(D, phi,lambda,N,phi_test)
+    W_average = zeros(size(phi_test,2),length(lambda));
     Y = cell(1,length(lambda));
     W = cell(1,length(lambda));
     for i = 1: length(lambda)
-        W(i) = mat2cell(WCal(D,phi,lambda(i),N),N,length(D));
+        W(i) = mat2cell(WCal(D,phi,lambda(i),size(phi_test,2))...
+            ,size(phi_test,2),length(D));
         Y(i) = mat2cell(YCal(W{i},phi,N,D),N,length(D));
         WCell = W{i};
         W_average(:,i) = mean(WCell,2);
+        Y_test(:,i) = phi_test*W_average(:,i);
     end
     
 end
 %% WCal: This function calculates Feature Vector (W)
-function W = WCal(D,phi,lambda,N)
-W = ones(N,length(D));
+function W = WCal(D,phi,lambda,Mu)
+W = ones(Mu,length(D));
 for i =1:length(D)
     T = cell2mat(D(i));
     T = T(:,2);
     X = cell2mat(phi(:,i));
-    W(:,i) = inv((X')*X+lambda*eye(length(X)))*X'*T;
+    W(:,i) = inv((X')*X+lambda*eye(Mu))*X'*T;
 end
 end
 %% YCal: This function calculates Y
@@ -101,36 +110,19 @@ Var = zeros(length(W_average),1);
    end
    BiasSq = BiasSq/(N);
    Var = Var/(N*length(D)*j/7);
+   %*j/7
    BiasSq_Var = BiasSq + Var;
   
 end
-
-%% 
-function plt = pltLine(D,Y,Y_average)
-figure
-hold on
-
-for i = 1:length(D)
-    x = D{i};
-    y = Y(:,i);
-    sorted=sortrows([(x(:,1)) y]);
-    sorted_x = sorted(:,1);
-    sorted_y = sorted(:,2);
-    fitx=linspace(0,4,100);
-    fity = interp1(sorted_x,sorted_y,fitx,'spline');
-    line(fitx,fity,'Color','r','LineWidth',0.5);
-end
-    %figure
-    %Y_average = mean(Y,2);
-%     sorted=sortrows([(x(:,1)) Y_average]);
-%     sorted_x = sorted(:,1);
-%     sorted_y = sorted(:,2);
-%     fitx=linspace(0,4,100);
-%     fity = interp1(sorted_x,sorted_y,fitx,'spline');
-%    line(fitx,fity,'Color','black','LineWidth',1.5);
-    %plot(Y_average,'Color','black','LineWidth',1.5);
-    xlim([0 1])
-    ylim([-2 2])
+%% CalError: Testing error calculation
+function Test_Error = CalError(Y_test,T_test,X_test)
+Test_Error = zeros(1,size(Y_test,2));
+ for j =1:size(Y_test,2)
+    for i=1:length(X_test)
+        Test_Error(j) = Test_Error(j)+(Y_test(i,j)-T_test(i))^2;
+    end
+ end
+ Test_Error = Test_Error/length(X_test);
 end
 %% plt: Plots the figure includig Bias Squared, Variance, and their addition
 function y = plt(BiasSq,Var,BiasSq_Var,lambda)
@@ -139,6 +131,7 @@ hold on
 plot(log(lambda), BiasSq ,'LineWidth',2);
 plot(log(lambda), Var,'LineWidth',2);
 plot(log(lambda), BiasSq_Var,'Color','magenta','LineWidth',2);
+%plot(log(lambda),Test_Error/9,'Color','black','LineWidth',2);
 xlim([-3 2]);
 ylim([0 0.15]);
 xticks(-3:2);
